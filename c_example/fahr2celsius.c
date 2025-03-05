4f05319dcecbf74c05e62c92c309866285610cbf
Leejaeyun123 <qazxczf@gmail.com>
2025-03-05 09:59:45 +0900

    asdfe

---
+++ c_example/fahr2celsius.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // 
+    int fahr;
+    double celsus;
+
+    fahr = 200;
+    celsius = 5.0 / 9.0 * (fhar -32);
+    printf("fahrenheit te")
+    return 0;
+}
