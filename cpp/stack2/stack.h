4cfb789a5b6ac67f76d95138739bf5429c3d03e6
Leejaeyun123 <qazxczf@gmail.com>
2025-04-17 16:34:20 +0900

    04-17

---
+++ cpp/stack2/stack.h
@@ -0,0 +1,29 @@
+#ifndef STACK_H
+#define STACK_H
+
+class Stack {
+private:
+    static const int STACK_SIZE;
+
+    int *pArr_;
+    int size_;
+    int tos_;
+
+    Stack(const Stack& );
+    Stack& operator=(const Stack& );
+public:
+    //Stack();
+    explicit Stack(int size = STACK_SIZE);
+    ~Stack();
+
+    int size() const;
+    int remain() const;
+
+    bool isEmpty() const;
+    bool isFull() const;
+
+    void push(int data);
+    int pop();
+};
+
+#endif
