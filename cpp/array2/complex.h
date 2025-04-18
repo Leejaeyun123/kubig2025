a19b680c8d1d52ff58d9d8aa55a9c397e37dfb01
Leejaeyun123 <qazxczf@gmail.com>
2025-04-18 17:12:39 +0900

    04-18

---
+++ cpp/array2/complex.h
@@ -0,0 +1,49 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+#include <iostream>
+
+class Complex {
+friend std::ostream& operator<<(std::ostream& out, const Complex& rhs);
+
+private:
+    double re_;
+    double im_;
+
+public:
+    Complex(double re = 0.0, double im = 0.0);
+    //Complex(const Complex& ) { /* memberwise copy */}
+    //~Complex() {}
+
+    //Complex& operator=(const Complex& ) { /* memberwise copy */ return *this; }
+    Complex& operator+=(const Complex& rhs);
+    // +=, -=, *=, /= 0
+    // %=, &=, |=,  ^= X
+
+    bool operator==(const Complex& rhs) const;
+    bool operator!=(const Complex& rhs) const;
+    // >, <, >=, <=
+
+    const Complex operator+(const Complex& rhs) const;
+    const Complex operator-(const Complex& rhs) const;
+    // *, /
+    // % X
+
+    Complex& operator++();           // prefix
+    Complex operator++(int );        // postfix
+    // ++, -- X
+
+    // &, |, -, ^ X
+
+    // type casting X
+
+    //Complex *operator&() { return this; }
+    //const Complex *operator&() const { return this; }
+
+    double real() const;
+    double imag() const;
+
+    void real(double re);
+    void imag(double im);
+};
+
+#endif
