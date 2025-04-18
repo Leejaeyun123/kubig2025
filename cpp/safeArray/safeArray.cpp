821ba69ccaa01de68fe90e11279de5cac8f2a1b8
Leejaeyun123 <qazxczf@gmail.com>
2025-04-18 15:55:44 +0900

    04-18

--- cpp/safeArray/safeArray.cpp
+++ cpp/safeArray/safeArray.cpp
@@ -1,7 +1,5 @@
-#include <cassert>
 #include "safeArray.h"
-
-
+#include <cassert>
 SafeArray::SafeArray(int size)
 : Array(size)
 {
@@ -15,15 +13,16 @@ SafeArray::SafeArray(const int *pArr , int size)
 }
 
 SafeArray::SafeArray(const SafeArray& rhs)
-: Array((Array)rhs)
-{  //slicing하여 array관련 내용만 남게 타입캐스팅
+: Array((Array)rhs) // slicing!
+{
 
 }
+
 SafeArray& SafeArray::operator=(const SafeArray& rhs)
 {
-        if (this != &rhs){
+    if (this != &rhs){
         this->Array::operator=((Array)rhs);
-        // safearray 쪽에서 추가된게 있으면 여기에 추가
+        // if safeArray in add 치환해주면됨
     }
     return *this;
 }
@@ -35,7 +34,7 @@ bool SafeArray::operator==(const SafeArray& rhs)const
 
 int& SafeArray::operator[](int index)
 {
-    assert(index >= 0 && index < this->size()); //this->Array::size() 에서 Array::생략하여 표기
+    assert(index >= 0 && index < this->size());
     return this->Array::operator[](index);
 }
 
