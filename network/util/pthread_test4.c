6c922acb6f98adcbe5a1a69d3dc9e9310bad3400
Leejaeyun123 <qazxczf@gmail.com>
2025-05-02 14:48:32 +0900

    05-02

--- network/util/pthread_test4.c
+++ network/util/pthread_test4.c
@@ -8,12 +8,13 @@
 
 char *thread_main(void *arg);
 long long num = 0;
+pthread_mutex_t mtx;
 
 int main()
 {
     pthread_t t_id[THREAD_NUM];
     void *thr_ret;
-    int range[5][2] = {{1, 5}, {6, 10}, {10, 15}, {16, 20}, {21, 25}};
+    int range[5][2] = {{1, 5}, {6, 10}, {11, 15}, {16, 20}, {21, 25}};
 
     for (int i = 0; i < THREAD_NUM; ++i)
         pthread_create(&t_id[i], NULL, (void *)thread_main, (void *)&range[i]);
@@ -23,6 +24,7 @@ int main()
     {
         pthread_join(t_id[i], &thr_ret); // 쓰레드가 리턴 될때까지 대기!!
         printf("쓰레드에서 받아온 메세지: %s \n", (char *)thr_ret);
+        free(thr_ret);
     }
     printf("쓰레드 끝 메인 시작!! \n");
     for (int i = 0; i < 3; i++)
@@ -30,7 +32,6 @@ int main()
         sleep(1);
         puts("running main()");
     }
-    free(thr_ret);
     printf("num: %lld", num);
     return 0;
 }
@@ -46,9 +47,12 @@ char *thread_main(void *arg)
     {
         incre = -1;
     }
+
     for (int i = start * 1000000; i < end * 1000000; i++)
     {
-        num += incre;
+        pthread_mutex_lock(&mtx);
+        num = num + incre;
+        pthread_mutex_unlock(&mtx);
     }
     return msg;
 }
