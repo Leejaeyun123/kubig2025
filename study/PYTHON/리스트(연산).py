1ac2399b891705dddfd0df8878a1fb4934016df4
Leejaeyun123 <qazxczf@gmail.com>
2025-05-15 22:24:38 +0900

    05-15

---
+++ study/PYTHON/리스트(연산).py
@@ -0,0 +1,14 @@
+# 덧셈
+color1 = ['red', 'blue', 'green']
+color2 = ['orange', 'black', 'white']
+print(color1+color2)
+print(len(color1))
+total_color = color1+color2
+print(total_color)
+
+# 곱셈
+print(color1*2)
+
+# in 연산
+# 하나의 값이 해당 리스트에 들어있는지 확인
+print('blue' in color2)
