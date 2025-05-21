17b67eff8c260527721dfd3b178ec8473e401895
Leejaeyun123 <qazxczf@gmail.com>
2025-05-21 10:21:05 +0900

    05-21

--- study/file.c
+++ study/file.c
@@ -2,16 +2,12 @@
 
 int main(void)
 {
-int kor = 3, eng = 5, mat = 4; 
-int credits = kor + eng + mat;
-int res;
-double kscore = 3.8, escore = 4.4, mscore = 3.9;
-double grade = kscore + escore + mscore / 3;
+int a = 20, b = 3;
+double res;
 
-printf("전체 학점 : %d\n", credits);
-printf("평점의 평균 : %.1f\n", grade);
-res = (credits>=10) && (grade>4);
-printf("%d\n", res);
+res = ((double)a);
+printf("a : %d, b = %d\n", a, b);
+printf("a와 b의 결과 : %.1lf\n", res);
 
 return 0;
 }
