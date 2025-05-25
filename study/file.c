e7a5daf27c9ba85a47d3e9f89defdcbf662841cd
Leejaeyun123 <qazxczf@gmail.com>
2025-05-25 17:35:53 +0900

    05-25

--- study/file.c
+++ study/file.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 
 int main(void)
-{   
-    int i = 1, sum = 0;
-    
-    while (i <= 100)
-    {
-        if ((i % 3)==0)
-        {
-            continue;
-        }
-        sum += i;
-        i++;
-    }
-    
-    return 0;
+{
+	int num, pn;		
+	int cnt = 0;
+
+	printf("2 이상의 정수를 입력하세요 : ");
+	scanf("%d", &num);
+
+	for (int i = 2; i <= num; i++)
+	{
+		pn = 1;							
+		for (int j = 2; j < i; j++)		
+		{
+			if ((i % j) == 0)			
+			{
+				pn = 0;					
+				break;
+			}
+		}
+		if (pn == 1)					
+		{
+			printf("%3d", i);			
+			cnt++;						
+			if ((cnt % 5) == 0)
+				printf("\n");
+		}
+	}
+	return 0;
 }
+
+
