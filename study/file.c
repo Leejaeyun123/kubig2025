b5537b085aa3e6cb31e205eee2e47d5158d73886
Leejaeyun123 <qazxczf@gmail.com>
2025-05-25 22:58:39 +0900

    05-25

--- study/file.c
+++ study/file.c
@@ -2,32 +2,6 @@
 
 int main(void)
 {
-	int num, pn;		
-	int cnt = 0;
-
-	printf("2 이상의 정수를 입력하세요 : ");
-	scanf("%d", &num);
-
-	for (int i = 2; i <= num; i++)
-	{
-		pn = 1;							
-		for (int j = 2; j < i; j++)		
-		{
-			if ((i % j) == 0)			
-			{
-				pn = 0;					
-				break;
-			}
-		}
-		if (pn == 1)					
-		{
-			printf("%3d", i);			
-			cnt++;						
-			if ((cnt % 5) == 0)
-				printf("\n");
-		}
-	}
-	return 0;
+	int a = 10;
+	printf("~a : %d\n", ~a);
 }
-
-
