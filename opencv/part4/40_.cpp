8cc272e9fc9cc8eb5ceb70d2d371e031529a20c0
Leejaeyun123 <qazxczf@gmail.com>
2025-06-02 16:06:37 +0900

    06-02

--- opencv/part4/40_.cpp
+++
@@ -1,9 +0,0 @@
-// 카메라 켜서 frame 확보
-// 색 공간 -> hsv로 변경 clone()해서 따로 처리.
-// 양방향 필터 처리
-// inrange -> 색종이를 인식
-// morph 함수 -> 이진화 Mat 정리
-// labeling
-// 화면에 rectangle로 표시
-// putText, ft2로 색종이 or color-paper
-// 사각형과 폰트는 색종이를 따라다녀야 함!
