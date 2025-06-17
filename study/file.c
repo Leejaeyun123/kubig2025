b9dacfddf5a931c26a771855238cbc930c1225b1
Leejaeyun123 <qazxczf@gmail.com>
2025-06-17 17:29:20 +0900

    06-17

--- study/file.c
+++ study/file.c
@@ -2,16 +2,25 @@
 
 int main(void)
 {
-    int ary[5];
-
-    ary[0] = 10;
-    ary[1] = 20;
-    ary[2] = ary[0] + ary[1];
-    scanf("%d", &ary[3]);
-
-    printf("%d\n", ary[2]);
-    printf("%d\n", ary[3]);
-    printf("%d\n", ary[4]);
-
+    int score[5];
+    int i;
+    int total = 0;
+    double avg;
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &score[i]);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        total += score[i];
+    }
+    avg = total / 5.0;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%5d", score[i]);
+    }
+    printf("\n");
+    printf("평균 : %.1lf\n", avg);
+    
     return 0;
 }
